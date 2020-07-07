dc8e29b331acd35e36d42ca841bae65b16c22bd4
Laffery <2387065420@qq.com>
2020-07-07 16:32:12 +0800

    add footer

---
+++ src/components/footer.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="bottom center">
+        <div class="group">
+            <el-icon class="el-icon-info"></el-icon>
+            WeIns produced by Agent 007
+        </div>
+
+        <div class="member">
+            <el-link href="https://github.com/DDIP2020" target="_blank" type="primary">GitHub</el-link>
+            <el-divider direction="vertical"></el-divider>
+            <el-link href="https://github.com/Laffery" target="_blank" type="info">Ao Yuchen</el-link>
+            <el-divider direction="vertical"></el-divider>
+            <el-link href="https://github.com/1047666945" target="_blank" type="info">Dou Jiawei</el-link>
+            <el-divider direction="vertical"></el-divider>
+            <el-link href="https://github.com/TheJunhan" target="_blank" type="info">Xu Junhan</el-link>
+            <el-divider direction="vertical"></el-divider>
+            <el-link href="https://github.com/Tarosweet" target="_blank" type="info">Fu Yuhan</el-link>
+        </div>
+
+        <div class="copyright">
+            Copyright 2020© Powered by SJTU SE DDIP
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "footer"
+    }
+</script>
+
+<style scoped>
+    .bottom {
+        font-size: x-small;
+        color: #909399;
+        width: 1280px;
+        padding-bottom: 30px;
+        padding-top: 30px;
+        margin-left: -18px;
+        background-color: #F2F2F5;
+    }
+
+    .member {
+        margin-top: 5px;
+    }
+
+    .copyright {
+        margin-top: 5px;
+    }
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,13 +1,25 @@
 <template>
     <div>
-        <el-tag>Home</el-tag>
+        <el-header>
+            <el-tag>Top nav</el-tag>
+        </el-header>
+        <el-container style="height: 400px">
+            <el-tag>sub nav and main content</el-tag>
+        </el-container>
+        <el-footer>
+            <Footer></Footer>
+        </el-footer>
     </div>
 </template>
 
 <script>
-export default {
-    name: "home"
-}
+    import Footer from '../components/footer';
+
+    export default {
+        name: "home",
+        components: { Footer }
+    }
+
 </script>
 
 <style scoped>
