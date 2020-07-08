daa2fc36705fae1a254cf0acb927da95173c28f9
poker <1047666945@qq.com>
2020-07-08 09:53:18 +0800

    add searchnar & change ui

--- src/components/topnav.vue
+++ src/components/topnav.vue
@@ -1,16 +1,38 @@
 <template>
     <div>
         <div class="logo">
-            <img src="src/assets/image/login.png" alt="logo picture"></img>
+            <img class="logo img" src='../assets/image/logo.png' alt="logo picture" />
+        </div>
+        <div class="searchbar">
+            <Searchbar></Searchbar>
         </div>
     </div>
 </template>
 
 <script>
-
+    export default {
+        name: "Topnav"
+    }
 </script>
 
 
 <style scoped>
+ .logo{
+     width: 100%;
+     height:50px;
+     position: fixed;
+     background-color: red;
+     float: left;
+     margin:0;
+     padding: 0;
 
+ }
+ .logo .img{
+
+        width: 50px;
+    }
+ body{
+     margin:0;
+     padding:0;
+ }
 </style>
