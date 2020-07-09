534e5cfd077754d58848fe2fafef658c4313e2d5
Laffery <2387065420@qq.com>
2020-07-09 16:35:51 +0800

    add comment and share components

--- src/components/counter.vue
+++ src/components/counter.vue
@@ -1,42 +1,30 @@
 <template>
-
-        <el-card  shadow="hover">
-            <div class="totalcard">
-                <div class="cube" style="border-right:1px solid darkgray;">
-                    <el-button type="text"> {{counter.following}}</el-button>
-                    <div class="con" >关注</div>
-                </div>
-                <!--            <div class="cube">-->
-                <!--                <el-divider direction="vertical"></el-divider>-->
-                <!--            </div>-->
-                <div class="cube" style="border-right:1px solid darkgray;">
-                    <el-button type="text"> {{counter.follower}}</el-button>
-                    <div class="con">粉丝</div>
-                </div>
-                <!--            <div class="cube">-->
-                <!--                <el-divider direction="vertical"></el-divider>-->
-                <!--            </div>-->
-                <div class="cube">
-                    <el-button type="text" > {{counter.blogs}}</el-button>
-                    <div class="con">动态</div>
-                </div>
+    <el-card  shadow="hover">
+        <div class="card">
+            <div class="cube" style="border-right:1px solid darkgray;">
+                <el-button type="text"> {{counter.following}}</el-button>
+                <div class="con" >关注</div>
             </div>
-
-        </el-card>
-
-
+            <div class="cube" style="border-right:1px solid darkgray;">
+                <el-button type="text"> {{counter.follower}}</el-button>
+                <div class="con">粉丝</div>
+            </div>
+            <div class="cube">
+                <el-button type="text" > {{counter.blogs}}</el-button>
+                <div class="con">动态</div>
+            </div>
+        </div>
+    </el-card>
 </template>
 
 <script>
     export default {
-        name: "counter",
-        data()
-        {
+        data() {
             return{
                 counter:{
-                    following:12,
-                    follower:99,
-                    blogs:23
+                    following: 12,
+                    follower: 99,
+                    blogs: 23
                 }
             }
         }
@@ -44,26 +32,20 @@
 </script>
 
 <style scoped>
-    .totalcard {
+    .card {
         width: 100%;
         display: flex;
         background-color: white;
     }
-    .cube{
+
+    .cube {
         width: 33%;
         float: left;
         flex-direction: row;
 
     }
-    .con{
+
+    .con {
         color: darkgray;
     }
-    /*.dd {*/
-    /*    margin-top: 20%;*/
-    /*    color: darkgray;*/
-    /*    !*line-height: 50px;*!*/
-    /*    font-size: 40px;*/
-    /*    !*font-family: Arial, sans-serif;*!*/
-    /*    !*float: left;*!*/
-    /*}*/
 </style>
---
+++ src/components/share.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="share">
+        <div class="destination">
+            转发到
+        </div>
+        <div class="quote">
+            {{blog.content.text}}
+        </div>
+        <div class="comment">
+
+        </div>
+        <div class="footer">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "shareForm",
+        props: {
+            blog: {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .share {
+        background-color: white;
+    }
+</style>
