ff115296ccb97ea02f00dec2593a725621f51001
poker <1047666945@qq.com>
2020-07-15 22:20:49 +0800

    add signin and signup unit test to the project

---
+++ tests/unit/signinForm.spec.js
@@ -0,0 +1,147 @@
+import { shallowMount, createLocalVue, mount } from "@vue/test-utils";
+import ElementUI from "element-ui";
+import signinForm from "../../src/components/signinForm";
+
+
+const localVue = createLocalVue();
+localVue.use(ElementUI);
+
+/**
+ * mock的axios
+ * 可以根据需要添加其他函数功能
+ */
+const axios = {
+  get: async () => "success",
+  post:async ()=>"success"
+};
+
+
+describe("init", () => {
+  it("init state", () => {
+    const wrapper = shallowMount(signinForm, {
+      mocks: {
+        axios
+      },
+      localVue
+    });
+
+     /**
+     * 检查data的初始状态
+     */
+     expect(wrapper.vm.errmessage).toEqual("");
+    expect(wrapper.vm.checked).toEqual(false);
+    expect(wrapper.vm.isSubmit).toEqual(false);
+    expect(wrapper.vm.form).toEqual( {
+      phone: '',
+      password: '',
+    });
+    expect(wrapper.vm.rules).toEqual({
+      phone: [
+        {
+          required: true, message: '电话号码不能为空', trigger: 'blur'
+        },{
+          min: 11, max: 11, message: '电话号码为11位数字'
+        }],
+      password: [
+        {
+          required: true, message: '密码不能为空', trigger: 'blur'
+        },{
+          min: 6, max: 16, message: '密码长度在 6 到 16 个字符', trigger: 'blur'
+        }],
+    });
+    // /**
+    //  * 检查结果表格的初始状态
+    //  */
+    // expect(wrapper.find(".success").exists()).toBe(false);
+    // expect(wrapper.find(".error").exists()).toBe(false);
+    // /**
+    //  * 检查axios是否正确mock
+    //  */
+    // expect(wrapper.vm.axios.get()).resolves.toEqual("success");
+  });
+});
+
+
+describe("signinForm", ()=>{
+  const wrapper = mount(signinForm, {
+    mocks: {
+      axios
+    },
+    localVue
+  });
+
+
+    it("input loginFrom text", () => {
+    /**
+     * 找到输入框
+     */
+    const input_phone = wrapper.find("#signinForm_input_phone");
+    const input_password = wrapper.find("#signinForm_input_password");
+
+    expect(input_phone.exists()).toBe(true);
+    expect(input_password.exists()).toBe(true);
+    /**
+     * 修改输入的值，检查绑定的变量calculateText是否发生改变
+     */
+    input_phone.setValue("18796700152");
+    input_password.setValue("12345678");
+    expect(wrapper.vm.form.phone).toEqual("18796700152");
+    expect(wrapper.vm.form.password).toEqual("12345678");
+
+  });
+
+
+})
+
+describe("login and axios " ,async ()=>{
+      const wrapper = mount(signinForm, {
+      mocks: {
+        axios
+      },
+      localVue
+    });
+  it("login",  () => {
+
+    expect(wrapper.vm.isSubmit).toEqual(false);
+    expect(wrapper.vm.onSubmit()).resolves.toBeTruthy();
+    expect(wrapper.vm.isSubmit).toEqual(true);
+
+    wrapper.setData({
+      isSubmit:true
+    })
+    expect(wrapper.vm.onSubmit()).toEqual(false);
+    /**
+     * 需要等待下一次渲染后，才能检查DOM的变化
+     */
+
+  });
+  it("btn clicked",async ()=>{
+    const btn = wrapper.find("#signinForm_button_login");
+    expect(btn.exists()).toBe(true);
+    await btn.trigger('click');
+    expect(wrapper.vm.isSubmit).toEqual(true);
+  });
+  it("go to sign up",()=>{
+    expect(wrapper.vm.signup()).toBeTruthy();
+  })
+});
+
+// describe("router-link",()=>{
+//   const router = new VueRouter({ routes });
+//   const wrapper = mount(signinForm, {
+//     mocks: {
+//       axios
+//     },
+//     localVue
+//   });
+//   const wrapper2=shallowMount(App,{
+//     localVue,
+//     router
+//   })
+//   it('sign up btn',async ()=>{
+//     const btn = wrapper.find("#signinForm_button_signup");
+//     expect(btn.exists()).toBe(true);
+//     await btn.trigger("click");
+//     expect(wrapper2.find())
+//   })
+// });
---
+++ tests/unit/signupForm.spec.js
@@ -0,0 +1,229 @@
+import { shallowMount, createLocalVue, mount } from "@vue/test-utils";
+import ElementUI from "element-ui";
+import signupForm from "../../src/components/signupForm";
+
+
+const localVue = createLocalVue();
+localVue.use(ElementUI);
+
+const axios = {
+    get: async () => "success",
+    post:async ()=>"success"
+};
+/**
+ * mock的axios
+ * 可以根据需要添加其他函数功能
+ */
+
+
+// const Mock=require('mockjs');
+// Mock.mock('http://localhost:8088/user/reg',()=>{
+//     return ({
+//         data:"success"
+//     })
+//     }
+// )
+
+describe("init", () => {
+    it("init state", () => {
+        const wrapper = shallowMount(signupForm, {
+            mocks: {
+                axios
+            },
+            localVue
+        });
+
+        /**
+         * 检查data的初始状态
+         */
+        expect(wrapper.vm.errmessage).toEqual("");
+        expect(wrapper.vm.registerForm).toEqual({
+            phone    : '',
+            name     : '',
+            password : '',
+            birthday : '',
+            type     : 0, // 默认为普通用户
+            sex      : 1, // 默认为男（1）
+            avatar: {
+                base64: 'http://bpic.588ku.com/element_pic/01/55/09/6357474dbf2409c.jpg'
+            }
+        });
+    });
+});
+
+describe("test function",()=>{
+    const wrapper = shallowMount(signupForm, {
+        mocks: {
+            axios
+        },
+        localVue
+    });
+    it('test nonage',()=>{
+        expect (wrapper.vm.nonage(new Date('2020-07-24'.replace(/-/g,"/")))).toEqual(false);
+        expect (wrapper.vm.nonage(new Date('2006-07-15'.replace(/-/g,"/")))).toEqual(false);
+        expect (wrapper.vm.nonage(new Date('2006-07-14'.replace(/-/g,"/")))).toEqual(true);
+        expect (wrapper.vm.nonage(new Date('2006-06-14'.replace(/-/g,"/")))).toEqual(true);
+
+        expect (wrapper.vm.nonage(new Date('2019-07-24'.replace(/-/g,"/")))).toEqual(false);
+        expect (wrapper.vm.nonage(new Date('2002-07-24'.replace(/-/g,"/")))).toEqual(true);
+
+    });
+    it('test birth_format',()=>{
+        expect (wrapper.vm.birth_format(new Date('2020-07-24'.replace(/-/g,"/")))).toEqual('2020-07-24');
+        expect (wrapper.vm.birth_format(new Date('2002-07-02'.replace(/-/g,"/")))).toEqual('2002-07-02');
+        expect (wrapper.vm.birth_format(new Date('2020-11-24'.replace(/-/g,"/")))).toEqual('2020-11-24');
+    })
+
+});
+
+describe("test axios", async()=>{
+    const wrapper = mount(signupForm, {
+        mocks: {
+            axios
+        },
+        localVue
+    });
+    it("test sign up", ()=>{
+        wrapper.setData({
+            registerForm:{
+                phone    : '18796700156',
+                name     : 'aoyuche',
+                password : '12345678',
+                birthday : 'Tue Jul 05 2005',
+                type     : 0, // 默认为普通用户
+                sex      : 1, // 默认为男（1）
+                avatar: {
+                    base64: 'http://bpic.588ku.com/element_pic/01/55/09/6357474dbf2409c.jpg'
+                }
+            }
+        })
+        expect(wrapper.vm.registerForm).toEqual({
+            phone    : '18796700156',
+            name     : 'aoyuche',
+            password : '12345678',
+            birthday : 'Tue Jul 05 2005',
+            type     : 0, // 默认为普通用户
+            sex      : 1, // 默认为男（1）
+            avatar: {
+                base64: 'http://bpic.588ku.com/element_pic/01/55/09/6357474dbf2409c.jpg'
+            }
+        });
+        expect(wrapper.vm.register()).resolves.toBeTruthy();
+        wrapper.setData({
+            registerForm:{
+                phone    : '18796700156',
+                name     : 'aoyuche',
+                password : '12345678',
+                birthday : 'Tue Jul 05 2018',
+                type     : 0, // 默认为普通用户
+                sex      : 1, // 默认为男（1）
+                avatar: {
+                    base64: 'http://bpic.588ku.com/element_pic/01/55/09/6357474dbf2409c.jpg'
+                }
+            }
+        });
+        expect(wrapper.vm.register()).toEqual(false);
+        expect(wrapper.vm.errmessage).toEqual("未成年人不能注册账户！");
+
+        wrapper.setData({
+            registerForm:{
+                phone    : '187967001567',
+                name     : 'aoyuche',
+                password : '12345678',
+                birthday : 'Tue Jul 05 2002',
+                type     : 0, // 默认为普通用户
+                sex      : 1, // 默认为男（1）
+                avatar: {
+                    base64: 'http://bpic.588ku.com/element_pic/01/55/09/6357474dbf2409c.jpg'
+                }
+            }
+        });
+        expect(wrapper.vm.register()).toEqual(false);
+        expect(wrapper.vm.errmessage).toEqual("电话号码格式不正确!");
+
+        wrapper.setData({
+            registerForm:{
+                phone    : '',
+                name     : 'aoyuche',
+                password : '12345678',
+                birthday : 'Tue Jul 05 2002',
+                type     : 0, // 默认为普通用户
+                sex      : 1, // 默认为男（1）
+                avatar: {
+                    base64: 'http://bpic.588ku.com/element_pic/01/55/09/6357474dbf2409c.jpg'
+                }
+            }
+        });
+        expect(wrapper.vm.register()).toEqual(false);
+        expect(wrapper.vm.errmessage).toEqual("电话号码不能为空!");
+
+        wrapper.setData({
+            registerForm:{
+                phone    : '18796700152',
+                name     : '',
+                password : '12345678',
+                birthday : 'Tue Jul 05 2002',
+                type     : 0, // 默认为普通用户
+                sex      : 1, // 默认为男（1）
+                avatar: {
+                    base64: 'http://bpic.588ku.com/element_pic/01/55/09/6357474dbf2409c.jpg'
+                }
+            }
+        });
+        expect(wrapper.vm.register()).toEqual(false);
+        expect(wrapper.vm.errmessage).toEqual("用户名不能为空!");
+
+        wrapper.setData({
+            registerForm:{
+                phone    : '18796700152',
+                name     : 'dasdasdasdasdadsasd',
+                password : '12345678',
+                birthday : 'Tue Jul 05 2002',
+                type     : 0, // 默认为普通用户
+                sex      : 1, // 默认为男（1）
+                avatar: {
+                    base64: 'http://bpic.588ku.com/element_pic/01/55/09/6357474dbf2409c.jpg'
+                }
+            }
+        });
+        expect(wrapper.vm.register()).toEqual(false);
+        expect(wrapper.vm.errmessage).toEqual("用户名长度须在 3 到 12 个字符!");
+
+        wrapper.setData({
+            registerForm:{
+                phone    : '18796700152',
+                name     : 'dasda',
+                password : 'dddddddddddddddddddd',
+                birthday : 'Tue Jul 05 2002',
+                type     : 0, // 默认为普通用户
+                sex      : 1, // 默认为男（1）
+                avatar: {
+                    base64: 'http://bpic.588ku.com/element_pic/01/55/09/6357474dbf2409c.jpg'
+                }
+            }
+        });
+        expect(wrapper.vm.register()).toEqual(false);
+        expect(wrapper.vm.errmessage).toEqual("密码长度须在 6 到 16 个字符");
+
+
+        wrapper.setData({
+            registerForm:{
+                phone    : '18796700152',
+                name     : 'dasda',
+                password : '',
+                birthday : 'Tue Jul 05 2002',
+                type     : 0, // 默认为普通用户
+                sex      : 1, // 默认为男（1）
+                avatar: {
+                    base64: 'http://bpic.588ku.com/element_pic/01/55/09/6357474dbf2409c.jpg'
+                }
+            }
+        });
+        expect(wrapper.vm.register()).toEqual(false);
+        expect(wrapper.vm.errmessage).toEqual("密码不能为空");
+
+
+
+
+    })
+})
