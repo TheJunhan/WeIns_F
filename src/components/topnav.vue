3c2fbab5bbb9c878a8fa283b25ea1428bd94bd3c
Laffery <2387065420@qq.com>
2020-08-29 20:01:46 +0800

    :fire: FEAT: 顶部搜索栏以及标签的搜索

--- src/components/topnav.vue
+++ src/components/topnav.vue
@@ -5,31 +5,42 @@
                 <img class="logo img" src='../assets/image/logo.png' alt="logo picture"/>
                 <span class="tit">WeIns</span>
             </div>
+
             <div class="searchbar">
                 <el-autocomplete
+                        popper-class="my-autocomplete"
                         v-model="state"
                         :fetch-suggestions="querySearchAsync"
-                        placeholder="请输入内容"
+                        placeholder="请输入搜索内容"
                         @select="handleSelect"
-                        size="min"
                         style="width: 40%"
-                ></el-autocomplete>
-                <el-button type="primary" icon="el-icon-search" v-on:click="log(0)">搜索</el-button>
+                        @keyup.native.enter="querySearchAsync">
+                    <template slot-scope="{ item }">
+                        <div v-if="item.type === '用户'">
+                            <div class="name">{{ item.value }}</div>
+                            <span class="underline">{{ item.type }}</span>
+                        </div>
+                        <div v-else>
+                            <div class="topic">#{{ item.value }}#</div>
+                            <span class="underline">{{ item.type }}</span>
+                        </div>
+                    </template>
+                </el-autocomplete>
+                <el-button type="primary" icon="el-icon-search" @click="querySearch">搜索</el-button>
             </div>
+
             <div class="nav">
-
-
                 <div class="el-dropdown-link">
                     <router-link to="/home">
                         <p class="el-dropdown-link can-point"><i class="el-icon-s-home el-icon--left"></i>首页</p>
                     </router-link>
                 </div>
+
                 <span class="dd">|</span>
 <!--                <router-link to="/manager" class="el-dropdown-link">-->
                     <p class="el-dropdown-link can-point" ><i class="el-icon-search el-icon--left"></i>发现</p>
 <!--                </router-link>-->
                 <span class="dd" v-if="this.$root.logged === true">|</span>
-
 
                 <div class="el-dropdown-link" v-if="this.$root.logged === true">
                     <el-dropdown  trigger="click">
@@ -50,7 +61,6 @@
                     </el-dropdown>
                 </div>
 
-
                 <span class="dd">|</span>
                 <div class="el-dropdown-link">
                     <el-dropdown trigger="click">
@@ -65,7 +75,6 @@
                     </el-dropdown>
                 </div>
 
-
             </div>
         </div>
 
@@ -78,85 +87,134 @@
 
 <script>
     import Login from "./signinForm";
+    import axios from  'axios';
+
     export default {
         components: { Login },
         data() {
             return {
+                heat: [],
                 items: [],
                 state: '',
                 timeout: null,
                 activeIndex: '1',
                 activeIndex2: '1',
-                showitems:[],
-                dialogVisible: false,
-                any: [{
-                    value: 'huangjingao',
-                    label: '黄金糕',
-                }, {
-                    value: 'shizitou',
-                    label: '狮子头',
-                }, {
-                    value: 'luosifen',
-                    label: '螺蛳粉',
-                }]
+                dialogVisible: false
             };
         },
+        created() {
+            // 候选项，如果未进行搜索则候选项为热搜榜，故这里拉取热搜榜
+            this.heat = [
+                {"value": "三全鲜食（北新泾店）", type: '话题'},
+                {"value": "Hot honey 首尔炸鸡", type: '话题'},
+                {"value": "新旺角茶餐厅", type: '话题'},
+                {"value": "泷千家(天山西路店)", type: '话题'},
+                {"value": "胖仙女纸杯蛋糕", type: '话题'},
+                {"value": "贡茶", type: '话题'},
+                {"value": "豪大大香鸡排超级奶爸", type: '话题'},
+                {"value": "茶兰（奶茶，手抓饼）", type: '话题'}
+                ];
+        },
         methods: {
-            loadAll() {
-                // const url="https://localhost:8088/ddd"
-                // return this.axios.post(url).then(res=>{
-                //     return res === 'success';
-                // })
+            querySearch() {
+                if (this.state === '') {
+                    this.$message.info('搜索内容为空!');
+                    return false;
+                }
+
+                this.items = [];
+                this.querySearchUser();
+                this.querySearchLabel();
+
+                console.log('items');
+                console.log(this.items);
+                return true;
             },
-            querySearchAsync(queryString,cbe) {
+            querySearchUser() {
+                let url = 'http://localhost:8088/user/getByFuzzyName?name=' + this.state;
 
-                let items = this.items;
-                let results = queryString ? items.filter((state) => {
-                    return (state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
-                }) : items;
-                    clearTimeout(this.timeout);
-                    this.timeout = setTimeout(() => {
-                        cbe(results);
-                    }, 3000 * Math.random());
+                axios.get(url).then(res => {
+                    let users = res.data;
+                    for (let i = 0; i < users.length; ++i) {
+                        let user = {
+                            value: users[i].name,
+                            type: '用户',
+                            id: users[i].id
+                        };
+                        this.items.push(user);
+                    }
+                }).catch(err => {
+                    console.log(err);
+                });
+            },
+            querySearchLabel() {
+                let url = 'http://localhost:8088/blog/findFuzzyLabels?lab=' + this.state;
+                axios.get(url).then(res => {
+                    let labels = res.data;
+                    for (let i = 0; i < labels.length; ++i) {
+                        let label = {
+                            value: labels[i].content,
+                            type: '话题',
+                            id: labels[i].id
+                        };
+                        this.items.push(label);
+                    }
+                }).catch(err => {
+                    console.log(err);
+                });
+            },
+            querySearchAsync(queryStr, cbe) {
+                let results = this.heat; // 默认为热搜榜
+                if (queryStr) {
+                    this.querySearch();
+                    results = this.items;
+                }
 
-               return results;
+                clearTimeout(this.timeout);
+                this.timeout = setTimeout(() => { cbe(results); }, 1000 * Math.random());
+                return results;
             },
             handleSelect(item) {
-                console.log(item);
+                if (item.type === '用户') {
+                    if (this.$root.logged === true &&
+                        item.id === Number(sessionStorage.getItem("id"))) {
+                        this.$router.push('/person');
+                        return;
+                    }
+
+                    this.$router.push({
+                        path: '/visit',
+                        query: {
+                            id: item.id
+                        }
+                    });
+                }
+
+                else { // TO DO: 显示话题
+                    console.log(item);
+                }
+
                 return item;
             },
             log(i) {
-                if(i === 0){
-                    this.dialogVisible = true;
-                    return 0;
+                switch (i) {
+                    case 0:
+                        this.dialogVisible = true;
+                        return 0;
+                    case 1:
+                        this.$router.push("/signup");
+                        return 1;
+                    case 2: //注销
+                        this.$root.logout();
+                        this.$router.push("/home");
+                        sessionStorage.clear();
+                        this.$message.success("已注销");
+                        return 2;
+                    default:
+                        return -1;
                 }
-                else if (i === 1) {
-                    this.$router.push("/signup");
-                    return 1;
-                }
-                else if(i === 2){
-                    //注销
-                    this.$root.logout();
-                    this.$router.push("/home");
-                    sessionStorage.clear();
-                    this.$message.success("已注销");
-                    return 2;
-                }
-                else return -1;
             }
-        },
-        mounted() {
-
-            this.items=[{"value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号"},
-            {"value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号"},
-            {"value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113"},
-            {"value": "泷千家(天山西路店)", "address": "天山西路438号"},
-            {"value": "胖仙女纸杯蛋糕（上海凌空店）", "address": "上海市长宁区金钟路968号1幢18号楼一层商铺18-101"},
-            {"value": "贡茶", "address": "上海市长宁区金钟路633号"},
-            {"value": "豪大大香鸡排超级奶爸", "address": "上海市嘉定区曹安公路曹安路1685号"},
-            {"value": "茶芝兰（奶茶，手抓饼）", "address": "上海市普陀区同普路1435号"}];
-            this.loadAll();
-        },
+        }
     }
 </script>
 
@@ -166,7 +224,7 @@
         font-size: 20px;
         color: coral;
         line-height: 50px;
-        font-family: "Al Bayan",serif;
+        font-family: "Al Bayan", serif;
         width: 50%;
     }
     .container {
@@ -200,18 +258,11 @@
         line-height: 50px;
         text-align: center;
     }
-    el-autocomplete {
-        width: 10%
-    }
     .nav {
         width: 45%;
         float: left;
         text-align: center;
         height: 50px;
-    }
-    el-menu {
-        height: 50px;
-        background-color: antiquewhite;
     }
     .el-dropdown-link {
         font-size: 15px;
@@ -231,5 +282,30 @@
         font-size: 15px;
         font-family: Arial, sans-serif;
         float: left;
+    }
+</style>
+
+<style scoped>
+    .my-autocomplete li {
+        line-height: normal;
+        padding: 7px;
+    }
+    .name {
+        text-overflow: ellipsis;
+        overflow: hidden;
+        color: #67C667;
+    }
+    .topic {
+        text-overflow: ellipsis;
+        overflow: hidden;
+        color: #EE731B;
+    }
+    .underline {
+        margin-top: -5px;
+        font-size: 12px;
+        color: #b4b4b4;
+    }
+    .highlighted .addr {
+        color: #ddd;
     }
 </style>
